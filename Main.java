d2bcc3c6c3210997cd33c27616a7536703a28d74
kristoffer <j.kristoffer.bruun@gmail.com>
2025-01-23 14:05:18 +0100

    1's commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle sabb = new Saab95();
+        System.out.println(sabb.getNrDoors());
+        System.out.println(sabb.getEnginePower());
+        System.out.println(sabb.getCurrentSpeed());
+        System.out.println(sabb.getEnginePower());
+        System.out.println(sabb);
+
+    }
+}
--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,120 @@
-public class Vehicle {
-    
+
+import java.awt.*;
+
+public class Vehicle implements Moveable{
+    protected int nrDoors;
+    protected double enginePower;
+    protected double currentSpeed;
+    protected Color color;
+    protected String modelName;
+    protected int x = 0;
+    protected int y = 0;
+
+    public enum Direction {
+        FORWARD, BACKWARD, LEFT, RIGHT
+    }
+
+    Direction currentDirection = Direction.FORWARD;
+
+
+    public int getNrDoors(){
+        return this.nrDoors;
+    }
+
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public double speedFactor(){
+        return 0;
+    };
+
+    public void incrementSpeed(double amount){};
+
+    public void decrementSpeed(double amount){};
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+
+    @Override
+    public void move() {
+        switch (currentDirection) {
+            case RIGHT:
+                this.x += currentSpeed;
+                break;
+            case LEFT:
+                this.x -= currentSpeed;
+                break;
+            case BACKWARD:
+                this.y -= currentSpeed;
+                break;
+            case FORWARD:
+                this.y += currentSpeed;
+                break;
+        }
+    }
+
+    @Override
+    public void turnLeft() {
+        switch (currentDirection) {
+            case FORWARD:
+                currentDirection = Direction.LEFT;
+                break;
+            case LEFT:
+                currentDirection = Direction.BACKWARD;
+                break;
+            case BACKWARD:
+                currentDirection = Direction.RIGHT;
+                break;
+            case RIGHT:
+                currentDirection = Direction.FORWARD;
+                break;
+        }
+    }
+
+    @Override
+    public void turnRight() {
+        switch (currentDirection) {
+            case FORWARD:
+                currentDirection = Direction.RIGHT;
+                break;
+            case RIGHT:
+                currentDirection = Direction.BACKWARD;
+                break;
+            case BACKWARD:
+                currentDirection = Direction.LEFT;
+                break;
+            case LEFT:
+                currentDirection = Direction.FORWARD;
+                break;
+        }
+    }
+
 }
